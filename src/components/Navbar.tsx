51a9dd6ac6f80b60cc23709fd71ccf86f3696f05
hiuuuu <hieu.nguyenminh@hdwebsoft.dev>
2025-08-05 09:30:26 +0700

    complete UI header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,16 @@
 import { Link, useLocation } from "react-router-dom";
-// import { Dialog } from "./ui/dialog.tsx";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogClose,
+} from "./ui/dialog.tsx";
 
-import ThemeToggle from './ThemeToggle.tsx';
-import PATHS from '@/routers/paths';
+import ThemeToggle from "./ThemeToggle.tsx";
+import PATHS from "@/routers/paths";
 import { Menu } from "lucide-react";
 
 function Navbar() {
@@ -12,22 +20,63 @@ function Navbar() {
     const current = location.pathname;
     const isActive = current === path;
     return `px-3 py-2 rounded hover:bg-gray-200 dark:hover:bg-gray-200 ${
-      isActive ? "bg-gray-300 dark:bg-gray-300 font-semibold" : ""
+      isActive ? "bg-gray-300 dark:bg-gray-300 font-semibold text-white" : ""
     }`;
   };
 
   return (
     <nav className="p-4 bg-gray-100 flex gap-4 dark:text-black justify-between">
       <div className="hidden sm:flex flex gap-4">
-        <Link to={PATHS.HOME} className={linkClass(PATHS.HOME)}>Home</Link>
-        <Link to={PATHS.ABOUT} className={linkClass(PATHS.ABOUT)}>About</Link>
-        <Link to={PATHS.POSTS} className={linkClass(PATHS.POSTS)}>Post</Link>
+        <Link to={PATHS.HOME} className={linkClass(PATHS.HOME)}>
+          Home
+        </Link>
+        <Link to={PATHS.ABOUT} className={linkClass(PATHS.ABOUT)}>
+          About
+        </Link>
+        <Link to={PATHS.POSTS} className={linkClass(PATHS.POSTS)}>
+          Post
+        </Link>
       </div>
       <div className="sm:flex hidden">
         <ThemeToggle />
       </div>
-      <div className="sm:hidden block">
-        <Menu size={24} />
+      <div className="block sm:hidden">
+        <Dialog>
+          <DialogTrigger asChild>
+            <button aria-label="Open menu">
+              <Menu size={24} />
+            </button>
+          </DialogTrigger>
+
+          <DialogContent className="pointer-events-auto h-screen w-1/2 left-0 translate-x-0 transform -translate-y-1/2 sm:max-w-sm max-w-xs">
+            <div>
+              <DialogHeader className="h-fit">
+                <DialogTitle className="text-lg flex items-center justify-between dark:text-white">
+                  Menu
+                  <ThemeToggle />
+                </DialogTitle>
+              </DialogHeader>
+
+              <DialogDescription className="flex flex-col gap-2">
+                <DialogClose asChild>
+                  <Link to={PATHS.HOME} className={linkClass(PATHS.HOME) + ' text-center'}>
+                    Home
+                  </Link>
+                </DialogClose>
+                <DialogClose asChild>
+                  <Link to={PATHS.ABOUT} className={linkClass(PATHS.ABOUT) + ' text-center'}>
+                    About
+                  </Link>
+                </DialogClose>
+                <DialogClose asChild>
+                  <Link to={PATHS.POSTS} className={linkClass(PATHS.POSTS) + ' text-center'}>
+                    Post
+                  </Link>
+                </DialogClose>
+              </DialogDescription>
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
     </nav>
   );
